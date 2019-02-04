4740a3719399d4685a6970817c3421d071a08817
Ole <ole@arkitektum.no>
2019-02-04 10:25:29 +0100

    Open sans for default font

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import WebFont from 'webfontloader';
 
 WebFont.load({
     google: {
-        families: ['Titillium web:300,400,700', 'sans-serif']
+        families: ['Open Sans:400,600,700','sans-serif']
     }
 });
 
