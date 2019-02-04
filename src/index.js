2962f07ae6f68cb2f8ae89dde55c025e920ca283
Ole <ole@arkitektum.no>
2019-02-04 10:16:51 +0100

    Introducing webfontloader, removing unused fontfiles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import AddWmsPanel from './components/AddWmsPanel/AddWmsPanel';
 import BackgroundChooser from './components/BackgroundChooser/BackgroundChooser';
 import Map from './components/MapComponent/MapComponent';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Titillium web:300,400,700', 'sans-serif']
+    }
+});
 
 export { AddWmsPanel, BackgroundChooser, Map };
