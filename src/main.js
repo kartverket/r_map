6c6814d8ad74c2b2f60e8277b9ebf5565edf0002
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-06 14:54:42 +0100

    Add icons to main file

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import AddWmsPanel from './components/AddWmsPanel/AddWmsPanel';
 import BackgroundChooser from './components/BackgroundChooser/BackgroundChooser';
 import MapComponent from './components/MapComponent/MapComponent';
 
+import './Utils/icons';
+
 export { AddWmsPanel, BackgroundChooser, MapComponent };
