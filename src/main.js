d0ce676558664642aee8b2225d7244ff6c89bf75
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-06 14:09:25 +0100

    Add main file for module export

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import AddWmsPanel from './components/AddWmsPanel/AddWmsPanel';
+import BackgroundChooser from './components/BackgroundChooser/BackgroundChooser';
+import MapComponent from './components/MapComponent/MapComponent';
+
+export { AddWmsPanel, BackgroundChooser, MapComponent };
