bfb51ed5d3bca4ea560b8498e9d4b674d65b2c7a
Carsten Mielke <carsten.mielke@kartverket.no>
2019-02-15 14:40:44 +0100

    restructure

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,34 +1,19 @@
-//import "bootstrap/dist/css/bootstrap.css";
-//import "bootstrap/dist/css/bootstrap-theme.css";
 import React from "react";
-import { map, eventHandler, mapConfig, addLayer } from "../../MapUtil/maplibHelper";
-import { CapabilitiesUtil } from "../../MapUtil/CapabilitiesUtil";
-
-import { mergeDefaultParams, parseWmsCapabilities } from "../../Utils/MapHelper";
 import PropTypes from "prop-types";
+
+import { CapabilitiesUtil } from "../../MapUtil/CapabilitiesUtil";
+import { map, eventHandler, mapConfig } from "../../MapUtil/maplibHelper";
+
 import queryString from "query-string";
 import setQuery from "set-query-string";
-import BackgroundChooser from "../BackgroundChooser/BackgroundChooser";
-import AddWmsPanel from "../AddWmsPanel/AddWmsPanel";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from "./MapComponent.scss";
 
-class ListItem extends React.Component {
-  render() {
-    return (
-      <AddWmsPanel key="1" map={map} services={this.props.listItem} removeMapItem={this.props.removeMapItem} draggable />
-    );
-  }
-}
 /**
  * @class The Map Component
  * @extends React.Component
  */
 export class MapComponent extends React.Component {
-  state = {
-    layers: []
-  };
   /**
    * The prop types.
    * @type {Object}
@@ -63,17 +48,9 @@ export class MapComponent extends React.Component {
      */
     onMapViewChanges: PropTypes.func,
     /**
-     * @type {String}
-     */
-    wms: PropTypes.string,
-    /**
      * @type {Array}
      */
     services: PropTypes.arrayOf(PropTypes.object),
-    /**
-     * @type {Boolean}
-     */
-    menu: PropTypes.bool
   };
 
   static defaultProps = {
@@ -83,9 +60,7 @@ export class MapComponent extends React.Component {
     onChangeZoom: () => { },
     lon: 396722,
     lat: 7197860,
-    zoom: 4,
-    wms: "",
-    menu: true
+    zoom: 4
   };
 
   /**
@@ -95,12 +70,8 @@ export class MapComponent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.handleSelect = this.handleSelect.bind(this);
-
     this.state = {
-      activeKey: "1",
-      open: false,
-      menu: this.props.menu
+      activeKey: "1"
     };
 
     const queryValues = queryString.parse(window.location.search);
@@ -109,12 +80,9 @@ export class MapComponent extends React.Component {
     let lat = Number(queryValues["lat"] || props.lat);
     let zoom = Number(queryValues["zoom"] || props.zoom);
 
-    this.wms = queryValues["wms"] || "";
-    this.layers = Array(queryValues["layers"] || []);
     /*
     let wmts = Array(queryValues['wmts'] || [])
     let wfs = Array(queryValues['wfs'] || [])
-    let projectName = queryValues['project'] || 'norgeskart'
     let epsg = queryValues['epsg'] || 'EPSG:3857'
 */
     //  this.props = { lon: lon, lat: lat, zoom: zoom };
@@ -125,68 +93,15 @@ export class MapComponent extends React.Component {
     this.olMap = null;
   }
 
-  /**
-   *
-   */
   componentDidMount() {
-    if (this.props.wms) {
-      this.addWMS(this.wms, this.layers);
-    }
     this.olMap = map.Init("map", this.newMapConfig);
     map.AddZoom();
     map.AddScaleLine();
     eventHandler.RegisterEvent("MapMoveend", this.updateMapInfoState);
     this.props = { map: map };
+    this.addWMS();
   }
 
-  /**
-   *
-   */
-  addWMS_ = (url, layers) => {
-    if (url) {
-      let newUrl = mergeDefaultParams(url, {
-        service: "WMS",
-        request: "GetCapabilities"
-      });
-      fetch(newUrl)
-        .then(function (response) {
-          return Promise.resolve(response.text());
-        })
-        .then(function (text) {
-          let resultText = parseWmsCapabilities(text);
-          let { Service, Capability } = {
-            ...resultText
-          };
-          if (Capability) {
-            let layerConfig = {
-              type: "map",
-              name: Capability.Layer[0].Abstract,
-              url: Capability.Layer[0].url,
-              params: {
-                layers: layers,
-                format: "image/png"
-              },
-              guid: "1.temakart",
-              options: {
-                isbaselayer: "true",
-                singletile: "false",
-                visibility: "true"
-              }
-            };
-            let newLayerConfig = addLayer(Service.Name, layerConfig);
-            map.AddLayer(newLayerConfig);
-          } else {
-            // console.log('No capabilities!')
-          }
-        });
-    } else {
-      // console.log('No wms parameter given')
-    }
-  };
-
-  /**
-   *
-   */
   updateMapInfoState = () => {
     let center = map.GetCenter();
     const queryValues = queryString.parse(window.location.search);
@@ -198,67 +113,27 @@ export class MapComponent extends React.Component {
   };
 
   addWMS() {
-    CapabilitiesUtil.parseWmsCapabilities(
-      this.props.services.GetCapabilitiesUrl
-    )
+    this.props.services.forEach(service => {
+      CapabilitiesUtil.parseWmsCapabilities(service.GetCapabilitiesUrl)
       .then(CapabilitiesUtil.getLayersFromWmsCapabilties)
       .then(layers => {
+        if (service.addLayers.length > 0) {
+          let layersToBeAdded = layers.filter(
+            e => service.addLayers.includes(e.name)
+          )
+          layersToBeAdded.forEach(layer => map.AddLayer(layer))
+        }
         this.setState({
           wmsLayers: layers
         });
       })
-      .catch(() => alert("Could not parse capabilities document."));
-  }
-
-  renderServiceList() {
-    return this.props.services.map((listItem, i) => {
-      return <ListItem listItem={listItem} removeMapItem={this.props.removeMapItem ? this.props.removeMapItem : null} key={i} map={map} />;
-    });
-  }
-  renderLayerButton() {  
-   return this.props.services && this.props.services.length > 0
-  }
-  handleSelect(activeKey) {
-    this.setState({
-      activeKey
-    });
-  }
-
-  toogleLayers() {
-    this.setState({
-      isExpanded: !this.state.isExpanded
+      .catch(e => console.warn(e));
     })
-
-  }
-  toogleMap() {
-    console.log('lukke kartet');
-    window.history.back();
-    // TODO: get paramtere to check for url til goto for closing map
   }
 
-  /**
-   *
-   */
   render() {
     return (
       <div className={style.mapContainer}>
-        <BackgroundChooser map={map} />
-        <div>
-          {this.renderLayerButton() ? (
-            <div className={this.state.isExpanded ? style.container + ' closed' : style.container + ' open'}>
-            <FontAwesomeIcon onClick={() => this.toogleLayers()} className={style.toggleBtn} icon={this.state.isExpanded ? ['far', 'layer-group'] : 'times' } />
-            <div>
-              
-              {this.renderServiceList()}
-            </div>
-          </div>
-          ) : (
-            <div>Gå til kartkatalogen</div>
-          )}
-          
-          
-          <div className={style.closeMap}><FontAwesomeIcon title="Lukk kartet" onClick={() => this.toogleMap()} className={style.toggleBtn} icon={'times'} /><span className={style.closeButtonLabel}>Lukk kartet</span></div>
-        </div>
         <div
           id="map"
           style={{
---
+++ src/components/MapContainer/MapConatiner.md
@@ -0,0 +1,29 @@
+This example shows the usage of the MapContainer
+
+```jsx
+const TEST_DATA = [
+  {
+    "Uuid": "8c2c434b-07f7-4ebc-9bc6-9c15cdd75c4c",
+    "Title": "Fastmerker & Basestajoner WMS",
+    "DistributionProtocol": "OGC:WMS",
+    "GetCapabilitiesUrl": "https://openwms.statkart.no/skwms1/wms.fastmerker2?request=GetCapabilities&service=WMS",
+    addLayers:[]
+  },
+  {
+    "Uuid": "8045628b-230a-4ba4-a6e0-xxxxx",
+    "Title": "Abas",
+    "DistributionProtocol": "OGC:WMS",
+    "GetCapabilitiesUrl": "https://openwms.statkart.no/skwms1/wms.adm_enheter?request=GetCapabilities&service=WMS",
+    addLayers:[]
+  },
+  {
+    "Uuid": "test-07f7-4ebc-9bc6-9c15cdd75c4c",
+    "Title": "norgeskart",
+    "DistributionProtocol": "OGC:WMTS",
+    "GetCapabilitiesUrl": "https://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?request=GetCapabilities&service=WMS",
+    addLayers:[]
+  }
+];
+
+<MapContainer services={TEST_DATA}/>
+```
