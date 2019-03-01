101d7ea57b2aa24704ea0f764a7c5b72fff5f8e0
Carsten Mielke <carsten.mielke@kartverket.no>
2019-03-01 15:28:28 +0100

    fix missing exports

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import './Utils/icons';
 import AddServicePanel from './components/AddServicePanel/AddServicePanel';
 import BackgroundChooser from './components/BackgroundChooser/BackgroundChooser';
 import MapComponent from './components/MapComponent/MapComponent';
+import MapContainer from './components/MapContainer/MapContainer';
+import Legend from './components/Legend/Legend';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
 
-export { AddServicePanel, BackgroundChooser, MapComponent };
+export { AddServicePanel, BackgroundChooser, MapComponent, MapContainer, Legend };
