cc1a24d6770cde3874c86bc86017e15e99e38553
Carsten Mielke <carsten.mielke@kartverket.no>
2020-05-22 09:30:44 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import MapComponent from './components/MapComponent/MapComponent';
 import MapContainer from './components/MapContainer/MapContainer';
 import Legend from './components/Legend/Legend';
 
-import './Utils/icons';
 import './index.scss'
 
 export { ServicePanel, BackgroundChooser, MapComponent, MapContainer, Legend };
